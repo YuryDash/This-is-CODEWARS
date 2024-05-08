d57b22493210f2b2b340201aa7299ac6d1b352ef
Yury Daszukiewicz <ydaszukiewicz@gmail.com>
2024-05-08 16:29:18 +0300

    08.05.2024

--- index7-8kyu.js
+++ index7-8kyu.js
@@ -730,3 +730,12 @@
   
 //   return res
 // }
+//==============================================================
+// A Letter's Best Friend
+// function bestFriend(txt, a, b) {
+//     for(let i = 0; i < txt.length; i++){
+//         if(txt[i] === a && txt[i + 1] !== b) return false
+//     }
+//     return true
+// }
+//==============================================================
